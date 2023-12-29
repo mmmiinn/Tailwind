31b5090b45a337ac99de3810f843182e1e1cdbb8
kimym <kimym@kimyoung.co.kr>
2023-12-29 11:35:58 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+
+### 실행하기
+
+```bash
+$ git clone https://github.com/mmmiinn/Tailwind.git
+$ npm
+$ npm dev
+```
+
+### tailwind 빌드하기
+
+아래 명령어를 통해 빌드를 실행합니다.
+
+```bash
+$ npm install -D tailwindcss
+$ npx tailwindcss init
+```
+
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,css}"],
+  theme: {
+    extend: {},
+  },
+  screens: {
+    'sm':'640px',
+    'md':'768px'
+  },
+  plugins: [],
+}
+
